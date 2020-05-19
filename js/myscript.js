9f45ebf9a12d34b891024d107ff6eeb9ce424792
Gianpiero <gpi91mx@gmail.com>
2020-05-19 14:44:41 +0200

    Conclusione progetto

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+
+alert('ALT STRANGER!!'); // ALL'AVVIO
+var nome = prompt('...and you are?');  //NOME
+var cognome = prompt("I see... your family name?");  // COGNOME
+var colorePreferito = prompt("I don't know why i'm ask it but...can you tell me your favorite color??"); // COLORE
+var numero = '19' // NUMERO
+
+document.getElementById('passwordpocosicura').innerHTML = nome + cognome + colorePreferito + numero; // CREAZIONE ELEMENTO PASSWORD
